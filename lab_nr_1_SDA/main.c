94c8508a4aeb58c7fb7888696e90987fffa19aab
Pavel1006 <79407164+Pavel1006@users.noreply.github.com>
2024-03-14 00:17:05 +0200

    laborator

---
+++ lab_nr_1_SDA/main.c
@@ -0,0 +1,184 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+
+typedef struct
+{
+    char title[50];
+    char author[50];
+    int number_of_pages;
+    float price;
+} Book;
+
+void generate_random_book(Book *book, int i)
+{
+    sprintf(book->title, "Random Title %d", i);
+    sprintf(book->author, "Random Author %d", i);
+    book->number_of_pages = rand() % 500 + 50;
+    book->price = (float)(rand() % 1000);
+}
+
+Book *generate_array_of_random_books(int size)
+{
+    Book *books = malloc(size * sizeof(Book));
+    if (books == NULL)
+    {
+        fprintf(stderr, "Memory allocation failed.\n");
+        exit(EXIT_FAILURE);
+    }
+
+    for (int i = 0; i < size; i++)
+    {
+        generate_random_book(&books[i], i);
+    }
+    return books;
+}
+
+void display_books(int size, Book *books)
+{
+    printf("Current Array:\n");
+    for (int i = 0; i < size; i++)
+    {
+        printf("Book %d: Title: %s, Author: %s, Pages: %d, Price: $%.2f\n", i + 1, books[i].title, books[i].author, books[i].number_of_pages, books[i].price);
+    }
+}
+
+int insert_book_by_index(Book **books, int *size, Book new_book, int insert_index)
+{
+    Book *new_array = realloc(*books, (*size + 1) * sizeof(Book));
+    if (new_array == NULL)
+    {
+        fprintf(stderr, "Memory allocation failed.\n");
+        return 0;
+    }
+    (*size)++;
+    *books = new_array;
+
+    if (insert_index < *size - 1)
+    {
+        for (int i = *size - 1; i > insert_index; i--)
+        {
+            (*books)[i] = (*books)[i - 1];
+        }
+    }
+
+    (*books)[insert_index] = new_book;
+
+    return 1;
+}
+
+int delete_book_at_index(Book **books, int *size, int delete_index)
+{
+    if (*size == 0 || delete_index < 0 || delete_index >= *size)
+    {
+        fprintf(stderr, "Invalid index for deletion.\n");
+        return 0;
+    }
+
+    for (int i = delete_index; i < *size - 1; i++)
+    {
+        (*books)[i] = (*books)[i + 1];
+    }
+
+    (*size)--;
+
+    if (*size > 0)
+    {
+        Book *new_array = realloc(*books, (*size) * sizeof(Book));
+        if (new_array == NULL)
+        {
+            fprintf(stderr, "Memory reallocation failed.\n");
+            return 0;
+        }
+        *books = new_array;
+    }
+    else
+    {
+        free(*books);
+        *books = NULL;
+    }
+
+    return 1;
+}
+
+void enter_book_details(Book *new_book)
+{
+
+    printf("Enter new book details (Title, Author, Pages, Price): ");
+    scanf("%s %s %d %f", new_book->title, new_book->author, &new_book->number_of_pages, &new_book->price);
+}
+
+int main()
+{
+    Book *books = NULL;
+    int size;
+    srand(time(NULL));
+    // size int verification
+    printf("Enter the number of random books to generate: ");
+    if (scanf("%d", &size) != 1)
+    {
+        fprintf(stderr, "Invalid input. Size should be an integer.\n");
+        return 0;
+    }
+    //
+    books = generate_array_of_random_books(size);
+
+    display_books(size, books);
+
+    int insert_index;
+
+    // insert index verification
+    printf("Enter insertion index: ");
+    if (scanf("%d", &insert_index) != 1)
+    {
+        fprintf(stderr, "Invalid input. Size should be an integer.\n");
+        return 0;
+    }
+    else if ((insert_index < 0) || (insert_index > size))
+    {
+        fprintf(stderr, "Number is invalid.\n");
+        return 0;
+    }
+    //
+    Book new_book;
+    enter_book_details(&new_book);
+
+    if (insert_book_by_index(&books, &size, new_book, insert_index))
+    {
+        printf("\nArray after insertion:\n");
+        display_books(size, books);
+    }
+    else
+    {
+        printf("Failed to insert new book.\n");
+    }
+
+    int delete_index;
+    printf("Enter deletion index: ");
+    // delete index int verify
+    if (scanf("%d", &delete_index) != 1)
+    {
+        fprintf(stderr, "Invalid input. Size should be an integer.\n");
+        return 0;
+    }
+    else if ((delete_index < 0) || (delete_index > size))
+    {
+        fprintf(stderr, "Number is invalid.\n");
+        return 0;
+    }
+    //
+    if (delete_book_at_index(&books, &size, delete_index))
+    {
+        printf("\nArray after deletion:\n");
+        display_books(size, books);
+    }
+    else
+    {
+        printf("Failed to delete book at index %d.\n", delete_index);
+    }
+
+    free(books);
+
+    return 0;
+}
